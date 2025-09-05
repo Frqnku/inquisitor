a93fbd42b8fa57b55a14bdd5baff55e4c857e3f3
Frqnku <ugo.tiberto@gmail.com>
2025-09-05 15:46:31 +0200

    feat: added a check on args to avoid having same src and target ip or mac address

--- inquisitor/src/parse_args.rs
+++ inquisitor/src/parse_args.rs
@@ -1,18 +1,25 @@
 use std::net::Ipv4Addr;
 
 use clap::Parser;
-use macaddr::MacAddr6;
+use pnet::util::MacAddr;
 
 #[derive(Parser, Debug)]
 pub struct Args {
     pub ip_src: Ipv4Addr,
-    pub mac_src: MacAddr6,
+    pub mac_src: MacAddr,
     pub ip_target: Ipv4Addr,
-    pub mac_target: MacAddr6,
+    pub mac_target: MacAddr,
 }
 
 impl Args {
     pub fn new() -> Self {
         Args::parse()
+    }
+
+    pub fn verify_args(&self) {
+        if (self.ip_src == self.ip_target) || (self.mac_src == self.mac_target) {
+            eprintln!("Error: Source and target IP/MAC addresses must be different.");
+            std::process::exit(1);
+        }
     }
 }
